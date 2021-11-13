181ac39a103f3d39184be38daa2979ecc0da49af
zighue1 <93688115+zighue1@users.noreply.github.com>
2021-11-13 16:51:35 -0300

    VERSAO2

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
 const btnAdd = document.querySelector('#btn-add')
 const inputTarefa = document.querySelector('#inputTarefa')
 const lista = document.querySelector('ul')
+let arrayTarefas = []
 
 function addItem(){
-    lista.innerHTML += ` <input type = "checkbox" onchange='riscaItem(this)'><p>${inputTarefa.value}</p><button onclick='remover(this)'>Remover</button><br>`
+    lista.appendChild(criaLi(inputTarefa.value, false))
+    inputTarefa.value = ''
     salva()
-}
+  }
 
 function riscaItem(e){
-    if(e.nextSibling.style.textDecoration !== 'line-through'){
-        e.nextSibling.style.textDecoration = 'line-through'
+    let p  = e.parentElement.querySelector('p')
+    for (let e of arrayTarefas) {
+        if(e.texto === p.textContent)
+            e.riscado = !e.riscado
+    }
+    if(p.className !== 'riscado'){
+        p.setAttribute('class','riscado')        
     }else{
-        e.nextSibling.style.textDecoration = 'none'
+        p.classList.remove("riscado");
     }
     salva()
 }
 
 function remover(e){
-    e.previousSibling.previousSibling.remove()
-    e.previousSibling.remove()
-    e.remove()
-    salva()
+    if(confirm('Tem certeza que deseja deletar a tarefa?')){
+        let p = e.parentElement.querySelector('p')
+        for (let index = 0; index < arrayTarefas.length; index++) {
+            const element = arrayTarefas[index];
+            if(element.texto === p.textContent){
+                arrayTarefas.splice(index, 1)
+                e.parentElement.remove()
+                salva()
+            }
+        }
+    }
 }
 
 function salva(){
-    localStorage.setItem('listaTarefas', lista.innerHTML)
+    localStorage.setItem('listaTarefas2', JSON.stringify(arrayTarefas))
 }
 
 window.onload = function(){
-    lista.innerHTML = localStorage.getItem('listaTarefas')
+    //localStorage.clear()
+
+    JSON.parse(localStorage.getItem('listaTarefas2')).forEach(e => {
+        lista.appendChild(criaLi(e.texto, e.riscado))
+    });
+ 
+ 
+}
+
+function criaLi(texto, checked){
+
+    let li = document.createElement('li')
+    let input = document.createElement('input')
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+
+    input.setAttribute('type','checkbox')
+    input.setAttribute('onchange','riscaItem(this)')
+    input.checked = checked
+    if(checked)
+        p.setAttribute('class', 'riscado')
+    p.textContent = texto
+    btn.setAttribute('onclick','remover(this)')
+    btn.innerText = 'Remover'
+    li.setAttribute('class','lContainer')
+
+    li.appendChild(input)
+    li.appendChild(p)
+    li.appendChild(btn)
+    
+    let objeto = {}
+    objeto.riscado = input.checked 
+    objeto.texto = p.textContent
+    arrayTarefas.push(objeto)
+
+    return li
 }
